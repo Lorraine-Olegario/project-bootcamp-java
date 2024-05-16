af4b5fe7405adb5036ac808e15ee213a707d2653
LOLO-BELEU <58961222+Lorraine-Olegario@users.noreply.github.com>
2024-05-15 22:37:07 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+
+public class ParametrosInvalidosException extends Throwable {
+
+    public ParametrosInvalidosException(String message) {
+        //System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        super(message);
+    }
+}
